0e23ff12f4a68a6454ac22e439c33383a543f739
elforgaani <mohamed.elforgani1999@gmail.com>
2024-05-01 22:58:43 +0200

    Add ReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import App from './App.tsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/app_router.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </QueryClientProvider>
+
   // <App />
 )
