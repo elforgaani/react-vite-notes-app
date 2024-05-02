7cacd017daa07fd49f05a5a8f4bcc4c29f63acc3
elforgaani <mohamed.elforgani1999@gmail.com>
2024-05-02 23:19:37 +0200

    Refactor main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/app_router.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast';
+import { RecoilRoot } from 'recoil';
+
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}>
+    <RecoilRoot>
       <App />
-    </RouterProvider>
+      <Toaster />
+    </RecoilRoot>
   </QueryClientProvider>
 
-  // <App />
 )
