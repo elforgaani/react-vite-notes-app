42d132dfa8bbd237d6241460fb3bf45c730fd2f4
elforgaani <mohamed.elforgani1999@gmail.com>
2024-05-03 15:41:56 +0200

    Add NoteModel.tsx

---
+++ src/components/notes/AddNoteModel.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useRef, useState } from 'react'
+import useAddNote from '../../hooks/useAddNote';
+import { useQueryClient } from '@tanstack/react-query';
+
+const AddNoteModel = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+
+    const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setContent(e.target.value)
+    }
+
+    const { mutate, isPending, error, isSuccess } = useAddNote()
+    const closeBtnRef = useRef<HTMLButtonElement>(null);
+    const queryClient = useQueryClient();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        mutate({ title, content }, {
+            onSuccess: () => {
+                queryClient.invalidateQueries({ queryKey: ['notes'] });
+                closeBtnRef.current?.click();
+            }
+        })
+    }
+
+    return <>
+        <dialog id="add-note-modal" className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box flex flex-col items-center gap-5 text-black">
+                <input type="text" placeholder='Title' className='input input-bordered w-full max-w-xs' value={title} onChange={handleTitleChange} />
+                <textarea value={content} placeholder='Content' onChange={handleContentChange} className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
+                <div className="modal-action">
+                    <button onClick={handleSubmit} disabled={!title || !content} className="btn btn-primary">{isPending ?  <span className="loading loading-dots loading-lg"></span> : 'Add Note'}</button>
+                    <form method="dialog">
+                        <button ref={closeBtnRef} className="btn btn-error">Close</button>
+                    </form>
+                </div>
+            </div>
+        </dialog>
+    </>
+}
+
+export default AddNoteModel
