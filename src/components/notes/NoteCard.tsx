de9205b7ab34f7118010e6a1087aaa3c3fcc739a
elforgaani <mohamed.elforgani1999@gmail.com>
2024-05-03 15:42:25 +0200

    Refactor NoteCard

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -1,12 +1,19 @@
-import React, { ReactNode } from 'react'
+import React, { ChangeEvent, ReactNode, useState } from 'react'
 import Note from '../../interfaces/note'
+import ModifyNoteModal from './ModifyNoteModal';
 
 const NoteCard = ({ note }: { note: Note }): ReactNode => {
+
+
+
+
     return <>
-        <div className="rounded-xl shadow-md w-70 h-52 m-3 flex flex-col cursor-pointer hover:bg-slate-200 hover:scale-[1.1] transition-all ">
+        <div onClick={() => document.getElementById('my_modal_5')!.showModal()} className="rounded-xl shadow-xl w-70 h-52 m-3 flex flex-col cursor-pointer hover:bg-slate-200 hover:scale-[1.1] transition-all ">
             <h4 className="text-center whitespace-nowrap overflow-hidden text-ellipsis px-6 pt-6">{note.title}</h4>
             <div className="divider"></div>
             <p className="overflow-hidden whitespace-nowrap text-ellipsis px-6 pb-6">{note.content}</p>
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <ModifyNoteModal note={note} />
         </div>
     </>
 }
