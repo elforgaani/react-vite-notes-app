b4afd0964fc6a76568254b779bbadcf37acf9eda
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-05-13 10:59:00 +0200

    Refactor AddNoteModel.tsx

--- src/components/notes/AddNoteModel.tsx
+++ src/components/notes/AddNoteModel.tsx
@@ -34,7 +34,7 @@ const AddNoteModel = () => {
                 <input type="text" placeholder='Title' className='input input-bordered w-full max-w-xs' value={title} onChange={handleTitleChange} />
                 <textarea value={content} placeholder='Content' onChange={handleContentChange} className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
                 <div className="modal-action">
-                    <button onClick={handleSubmit} disabled={!title || !content} className="btn btn-primary">{isPending ? <Loading /> : 'Add Note'}</button>
+                    <button onClick={handleSubmit} disabled={!title || !content || isPending} className="btn btn-primary">{isPending ? <Loading /> : 'Add Note'}</button>
                     <form method="dialog">
                         <button ref={closeBtnRef} className="btn btn-error">Close</button>
                     </form>
