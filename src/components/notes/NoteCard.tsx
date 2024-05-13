37411882e41e04badef04460fbc5eab4fc9c2452
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-05-13 10:59:37 +0200

    Refactor NoteCard

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -1,20 +1,20 @@
-import React, { ChangeEvent, ReactNode, useState } from 'react'
+
+import { ReactNode, useState } from 'react'
 import Note from '../../interfaces/note'
 import ModifyNoteModal from './ModifyNoteModal';
 
 const NoteCard = ({ note }: { note: Note }): ReactNode => {
-
-
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 
     return <>
-        <div onClick={() => document.getElementById('my_modal_5')!.showModal()} className="rounded-xl shadow-xl w-70 h-52 m-3 flex flex-col cursor-pointer hover:bg-slate-200 hover:scale-[1.1] transition-all ">
+        <div onClick={() => setIsModalOpen(true)
+        } className="rounded-xl shadow-md w-70 h-52 m-3 flex flex-col cursor-pointer hover:bg-slate-200 hover:scale-[1.05] transition-all ">
             <h4 className="text-center whitespace-nowrap overflow-hidden text-ellipsis px-6 pt-6">{note.title}</h4>
             <div className="divider"></div>
             <p className="overflow-hidden whitespace-nowrap text-ellipsis px-6 pb-6">{note.content}</p>
-            {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <ModifyNoteModal note={note} />
         </div>
+        <ModifyNoteModal note={note} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </>
 }
 
